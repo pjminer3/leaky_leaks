2f4648ff008875fe200e6dc8425228656f965aeb
Patrick Miner <patrickminer318@gmail.com>
2024-02-19 19:44:56 -0800

    test

--- arrays_and_strings/numSubarrayProductLessThanK.ts
+++ arrays_and_strings/numSubarrayProductLessThanK.ts
@@ -1,7 +1,7 @@
 /**
  * 
  * Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
- * 
+ * test
  */
 
 function numSubarrayProductLessThanK(nums: number[], k: number): number {
