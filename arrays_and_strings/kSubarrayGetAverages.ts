1a19a6174191f1a8de400aa29a1447d04ff53058
Patrick Miner <patrickminer318@gmail.com>
2024-02-24 18:49:15 -0800

    sliding window solution

--- arrays_and_strings/kSubarrayGetAverages.ts
+++ arrays_and_strings/kSubarrayGetAverages.ts
@@ -1,3 +1,20 @@
-function getAverages(nums: number[], k: number): number[] {
-    // solve it both ways. What are they?
+function getAveragesWithPrefixSum(nums: number[], k: number): number[] {
+    const windowSize = k * 2 + 1;
+    const solution = new Array(nums.length).fill(-1);
+    const prefixSum = [0];
+
+    for (let i = 0; i < nums.length; i++) {
+        prefixSum.push(prefixSum[i] + nums[i]);
+    }
+
+    for (let i = k; i++; i <= nums.length - 1 - k) {
+        solution[i] = Math.floor((prefixSum[i + k + 1] - prefixSum[i - k]) / windowSize);
+    }
+
+    return solution;
+};
+
+
+function getAveragesWithSlidingWindow(nums: number[], k: number): number[] {
+
 };
