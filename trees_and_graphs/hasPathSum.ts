3d1e41a2fc01d3cd207187deedd613faddbed016
Patrick Miner <patrickminer318@gmail.com>
2024-02-26 13:18:03 -0800

    iterative solution

--- trees_and_graphs/hasPathSum.ts
+++ trees_and_graphs/hasPathSum.ts
@@ -21,3 +21,36 @@ function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
     const newVal = targetSum - root.val;
     return hasPathSum(root.left, newVal) || hasPathSum(root.right, newVal);
 };
+
+function hasPathSumIterative(root: TreeNode | null, targetSum: number): boolean {
+    if (!root) {
+        return false;
+    }
+
+    const stack: [TreeNode, number][] = [[root, targetSum]];
+
+    while (stack.length) {
+        let poppedNode = stack.pop();
+
+        if (!poppedNode) {
+            break;
+        }
+
+        let [node, newTargetSum] = poppedNode;
+
+        if (!node.left && !node.right) {
+            if (node.val === newTargetSum) {
+                return true;
+            }
+        }
+
+        if (node.left) {
+            stack.push([node.left, newTargetSum - node.val]);
+        }
+        if (node.right) {
+            stack.push([node.right, newTargetSum - node.val]);
+        }
+    }
+
+    return false
+}
