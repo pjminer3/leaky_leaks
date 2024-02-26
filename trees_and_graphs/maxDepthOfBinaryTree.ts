0e8ceb3eb6ff58d6dfe4aca3c2464411c1e74c16
Patrick Miner <patrickminer318@gmail.com>
2024-02-25 21:35:06 -0800

    max depth of dfs of binary tree

---
+++ trees_and_graphs/maxDepthOfBinaryTree.ts
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+/**
+ * Given the root of a binary tree, return its maximum depth.
+ * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ */
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val ?? 0;
+        this.left = left ?? null;
+        this.right = right ?? null;
+    }
+}
+
+function maxDepth(root: TreeNode | null): number {
+    if (!root) {
+        return 0;
+    }
+
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
