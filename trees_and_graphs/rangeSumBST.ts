137ecbb14e94e149e24a872c317d5ce9a5d91c19
Patrick Miner <patrickminer318@gmail.com>
2024-06-21 18:20:24 -0700

    new rangeSumBST solution

--- trees_and_graphs/rangeSumBST.ts
+++ trees_and_graphs/rangeSumBST.ts
@@ -1,31 +1,22 @@
 function rangeSumBST(root: TreeNode | null, low: number, high: number): number {
     let sum = 0;
 
-    if (!root) {
-        return sum;
-    }
-
-    let queue: TreeNode[] = [root];
-
-    while (queue.length) {
-        let newQueue: TreeNode[] = [];
-        for (let i = 0; i < queue.length; i++) {
-            let node = queue[i];
-            if (node.val >= low && node.val <= high) {
-                sum += node.val;
-            }
-
-            if (node.val > low) {
-                if (node.left) newQueue.push(node.left);
-            }
-
-            if (node.val < high) {
-                if (node.right) newQueue.push(node.right);
-            }
+    function dfs(root: TreeNode): void {
+        if (!root ) {
+            return;
         }
 
-        queue = newQueue;
+        if (root.val > high) {
+            dfs(root.left);
+        } else if (root.val < low) {
+            dfs(root.right);
+        } else {
+            sum += root.val;
+            dfs(root.left);
+            dfs(root.right);
+        }
     }
+    dfs(root);
 
     return sum;
 };
